8138c3cb2fa30cbf8090ae1abece46f451640ca3
devzons <devzons@gmail.com>
2018-11-07 10:24:05 -0800

    Movie API test

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# WEB API
+## WEB API
 
 * Facebook API
 * GooglePlaces API
@@ -6,3 +6,38 @@
 * Twitter API
 * Weather API
 * Yelp API
+
+
+## Search for Podcasts about "code"
+https://itunes.apple.com/search?tern=code&entity=podcast
+
+## Search for Harry Potter Movies
+https://itunes.apple.com/search?term=harray-potter&entity=movie
+
+## Request thru javascript code
+```
+var request = require('request');
+request('http://www.reddit.com', function(error, response, body) {
+  if(error){
+    console.log("ERROR!!!");
+    console.log(error);
+  } else {
+    if(response.statusCode == 200){
+      console.log(body);
+    }
+  }
+});
+```
+## Sunset time 
+
+https://developer.yahoo.com/weather/
+
+## Movie API
+
+General search: 
+http://www.omdbapi.com/?s=guardians+of+the+galaxy&apikey=thewdb
+Search with Movie ID: 
+http://www.omdbapi.com/?i=tt3896198&apikey=thewdb
+
+you must append &apikey=thewdb to the end of your url
+
--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-var request = require('request');
-console.log("Sunset in Hawaii is at ...");
-request('https://query.yahooapis.com/v1/public/yql?q=select%20astronomy.sunset%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22maui%2C%20hi%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys', function(error, response, body) {
-  if(!error && response.statusCode == 200){
-    var parsedData = JSON.parse(body);
-    console.log(parsedData["query"]["results"]["channel"]["astronomy"]["sunset"]);
-  } 
+var express = require("express");
+var app = express();
+var request = require("request");
+
+app.get("/results", function(req, res){
+  request('http://www.omdbapi.com/?s=guardians+of+the+galaxy&apikey=thewdb', function(error, response, body){
+    if(!error && response.statusCode == 200){
+      var parsedData = JSON.parse(body);
+      res.send(parsedData["Search"][0]["Title"]);
+    }
+  });
+});
+
+
+app.listen(3000, function(req, res) {
+  console.log("Movie App has started!!!");
 });
